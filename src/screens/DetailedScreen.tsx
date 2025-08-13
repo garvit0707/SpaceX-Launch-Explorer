d3c96db4bda1d4e4d3e138ef737614e57e0518a1
garvit <garvit.sharma.0707@gmail.com>
2025-08-13 17:47:56 +0530

    Listing Design

--- src/screens/DetailedScreen.tsx
+++ src/screens/DetailedScreen.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {View,Text} from "react-native";
+import { useRoute } from "@react-navigation/native";
 
 
 const DetailedScreen = () => {
+  const route = useRoute();
+  const launch = route.params
+
+  console.log("the launch is @@@@@@@@@@@@@@@@@@@@@@@@@",launch)
   return (
     <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
       <Text>this is the Detailed screen i haev</Text>
